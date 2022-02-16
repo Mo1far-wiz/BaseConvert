77ee7afe7c2c063ac3099d9d66f3d1a505be7422
Molfar <sasha.prohor12@gmail.com>
2022-02-16 18:22:28 +0200

    fixed bug with float numbers starting with zero

--- BaseConvert/BaseConvert.cpp
+++ BaseConvert/BaseConvert.cpp
@@ -41,6 +41,8 @@ char BaseConvert::to_char(int n)
 std::string BaseConvert::integer_part_convert(const std::string& int_str, int to)
 {
 	long long decimal = std::stoll(int_str);
+	if ((int)decimal == 0)
+		return "0";
 	std::string converted_int_str = "";
 
 	for (int q = 0; decimal > 0; ++q) {
--- BaseConvert/Source.cpp
+++ BaseConvert/Source.cpp
@@ -4,6 +4,6 @@
 int main() {
 	BaseConvert converter;
 
-	std::string str = converter.convert("49cI2g8IC", 25, 34);
-	std::cout << str << std::endl;
+	std::cout << converter.convert("0.12122", 3, 9) << std::endl;
+
 }
